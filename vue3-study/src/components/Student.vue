69d824350c41b0c13bd85bd2d9499f7a554c1e4c
Asule <distinctofwang@gmail.com>
2026-09-13 22:19:47 +0800

    弄了下计算属性。

--- vue3-study/src/components/Student.vue
+++ vue3-study/src/components/Student.vue
@@ -7,17 +7,31 @@
     <button @click="changeAge">修改年龄</button>
     <button @click="changeHeight">修改身高</button>
   </div>
+  <hr />
+    <div class="person">
+      姓：<input type="text" v-model="firstName"> <br>
+      名：<input type="text" v-model="lastName"> <br>
+      全名：<span>{{ fullName }}</span> <br>
+      <button @click="changeFullName">修改计算属性</button>
+    </div>
 </template>
 <!-- 
 
   1、主要讲的是toRef和toRefs的使用
-  
+
+  2、什么是双向绑定?
+    页面上输入的值修改时，数据也会跟着修改，数据修改时，页面上显示的值也会跟着修改，这种现象就叫做双向绑定.
+
+
+  3、计算属性
+    （1）计算属性，计算时依赖的数据发生变化时，计算属性就会重新计算。
+     (2) 。
 
 -->
 
 
 <script lang="ts" setup name="Student">
-  import { reactive, toRef, toRefs } from 'vue'
+  import { reactive, toRef, toRefs,ref,computed } from 'vue'
 
   let person = reactive({
     name: '张三',
@@ -47,6 +61,36 @@
   function changeHeight() {
     height.value = 200
   }
+
+
+  let firstName = ref('')
+  let lastName = ref('')
+
+  // 计算属性，计算时依赖的数据发生变化时，计算属性就会重新计算
+  // 这种写法的计算属性是只读的，不可以直接修改值
+  // let fullName = computed(() => {
+  //   return firstName.value.slice(0, 1).toUpperCase() +
+  //     firstName.value.slice(1) +'-' +lastName.value
+  // })
+
+
+   // 此时计算属性是可读可写的，可以直接修改值
+   let fullName = computed({  
+    get() {
+      return firstName.value.slice(0, 1).toUpperCase() +firstName.value.slice(1) + '-' + lastName.value
+    },
+    set(newValue: string) {
+      let names = newValue.split('-')
+      firstName.value = names[0]
+      lastName.value = names[1]
+    } 
+  })  
+
+
+  const changeFullName = () => {
+    fullName.value = '王-五'
+  }
+
 </script>
 
 
