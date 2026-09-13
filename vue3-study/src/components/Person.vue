dfa357c4031eea7be540967f12b9a9bb40a2ded5
Asule <distinctofwang@gmail.com>
2026-09-13 21:14:29 +0800

    setup语法糖，使用vite-plugin-vue-setup-extend插件在setup上自定义组件名。

--- vue3-study/src/components/Person.vue
+++ vue3-study/src/components/Person.vue
@@ -12,47 +12,30 @@
 </template>
 
 <!-- 
-  script setup 会自动导出组件。直接import就可以使用。
-  setup函数中，不存在this。
--->
-<script lang="ts">
-    export default {
-        name: 'Person',
-        // 1、setup可以和data、methods共存，setup优先级高于data和methods。
-        // 2、data和methods中可以使用setup中返回的变量和方法。
-        data() {
-            return {
-                msg: 'i am person component,name is '+this.name+" age is "+this.age
-            }
-        },
-        methods: {
-            show() {
-                alert(`你好，我叫${this.name}，今年${this.age}岁`)
-            },
-        },
-        setup() {
-            // 这种方式声明变量，数据不是响应式的。修改数据后不会触发视图更新。
-            let name = '王五'
-            let age = 25
-            const sayHello = () => {
-                alert(`你好，我叫${name}，今年${age}岁`)
-            },
-            changeName = () => {
-                name = '李四'
-            },
-            changeAge = () => {
-                age = 30
-            }
-            return {
-                name,
-                age,
-                sayHello,
-                changeName,
-                changeAge
-            }
-        }
-    } 
+  
+  1、安装该插件后，可以使用name属性来定义组件名称，避免了在<script setup>中使用export default的方式来定义组件名称。
+     npm isntall vite-plugin-vue-setup-extend -D
+   
+  
+  -->
+<script setup lang="ts" name="PersonSuper">
+    // 这种方式声明变量，数据是响应式的。修改数据后会触发视图更新。
+    import { ref } from 'vue'
+    const name = ref('张三')
+    const age = ref(20)
+    const msg = ref('Hello Vue3 + TypeScript + Vite')
+    const sayHello = () => {
+        alert(`你好，我叫${name.value}，今年${age.value}岁`)
+    },
+    changeName = () => {
+        name.value = '赵六'
+    },
+    changeAge = () => {
+        age.value = 35
+    }
 </script>
+
+
 
 <style scoped>
 .person {
